d0aa742cc18d2ce07232a6f388809eb41745c88c
Assics99 <razvozzaevivan@gmail.com>
2025-04-14 19:36:51 +0900

    Update python_lesson6.py

--- python_lesson6.py
+++ python_lesson6.py
@@ -1,34 +1,29 @@
-print("Введите пароль: ", end = '')
-PASSWORD = input()
-def do_things():
-	def is_very_long(password):
-		return len(PASSWORD) > 12
-	def has_digits(password):
-		return any(character.isdigit() for character in PASSWORD)
-	def has_upper_letters(password):
-		return any(character.isupper() for character in PASSWORD)
-	def has_lower_letters(password):
-		return any(character.islower() for character in PASSWORD)
-	def has_symbol(password):
-		return any(not character.isdigit() and not character.isalpha() for character in PASSWORD)
+def is_very_long(password):
+	return len(password) > 12
+def has_digits(password):
+	return any(character.isdigit() for character in password)
+def has_upper_letters(password):
+	return any(character.isupper() for character in password)
+def has_lower_letters(password):
+	return any(character.islower() for character in password)
+def has_symbol(password):
+	return any(not character.isdigit() and not character.isalpha() for character in password)
+
+def main(): 
+	print("Введите пароль: ", end = '')
+	password = input()
 	score = 0
 	password_score = [
-		is_very_long(PASSWORD),
-		has_digits(PASSWORD),
-		has_upper_letters(PASSWORD),
-		has_lower_letters(PASSWORD), 
-		has_symbol(PASSWORD),
+		is_very_long(password),
+		has_digits(password),
+		has_upper_letters(password),
+		has_lower_letters(password), 
+		has_symbol(password),
 	]
 	for check in password_score:
 		if check:
 			score += 2
 	print("Рейтинг пароля:", score)
-
-def main(): 
-    do_things()
-
-if __name__ == '__main__':
-    main()
 
 if __name__ == '__main__':
     main()
