99a4a2bd04370aeb3309d995103f4d2cd5678cb5
Assics99 <razvozzaevivan@gmail.com>
2025-04-15 18:57:28 +0900

    Update python_lesson6.py

--- python_lesson6.py
+++ python_lesson6.py
@@ -1,14 +1,23 @@
 def is_very_long(password):
     return len(password) > 12
+
+
 def has_digits(password):
     return any(character.isdigit() for character in password)
+
+
 def has_upper_letters(password):
     return any(character.isupper() for character in password)
+
+
 def has_lower_letters(password):
     return any(character.islower() for character in password)
+
+
 def has_symbol(password):
     return any(not character.isdigit() and not character.isalpha() for character in password)
 	
+
 def main(): 
     print("Введите пароль: ", end = '')
     password = input()
